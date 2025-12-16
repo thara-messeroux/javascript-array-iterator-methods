f20a797909c1bf8f25aea6bb7b4fb200b879c467
thara-messeroux <tharamesseroux@gmail.com>
2025-12-15 21:37:27 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Hello from app.js');
+
+/* original array of instructors */
+const instructors = ['Beryl', 'Hunter', 'Joe', 'Jurgen', 'Ben', 'David'];
+
+/* use map to create a NEW array */
+const awesomeInstructors = instructors.map((instructor) => {
+    /* take one name and return a new string */
+    return `${instructor} is awesome`;
+});
+
+/* log the result so we can see it */
+console.log(awesomeInstructors);
