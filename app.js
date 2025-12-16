16095eaa6816318bd42b99b3938319850986798d
thara-messeroux <tharamesseroux@gmail.com>
2025-12-15 21:57:52 -0500

    Add filter() example to remove unwanted values

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 console.log('Hello from app.js');
 
-// map() You Do
+/* 
+STEP 2 — map()(Transforming Arrays Safely)
+🎯 Skill unlocked
+
+Turning one array into a new array without breaking anything
+map() You Do
+*/
+
 /* original array of instructors */
 const instructors = ['Beryl', 'Hunter', 'Joe', 'Jurgen', 'Ben', 'David'];
 
@@ -12,3 +19,25 @@ const awesomeInstructors = instructors.map((instructor) => {
 
 /* log the result so we can see it */
 console.log(awesomeInstructors);
+
+
+/* 
+🐶 STEP 3 — filter()(Selecting, not transforming)
+🎯 Skill unlocked
+
+Creating a new array by keeping only what matches a rule  */
+
+/* original array of people */
+const people = ['jerks', 'nice people', 'jerks', 'nice people', 'nice people'];
+
+/* filter out jerks */
+const notJerks = people.filter((person) => {
+    /* keep only items that are NOT 'jerks' */
+    return person !== 'jerks';
+});
+
+/* log the filtered result */
+console.log(notJerks);
+
+
+
